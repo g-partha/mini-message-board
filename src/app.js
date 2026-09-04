6ddc382504727240fbee6d5b290eee92b5b7fc6d
Partha Protim Gogoi <176602154+g-partha@users.noreply.github.com>
2026-09-04 17:02:36 +0530

    feat: Initial setup

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('node:path');
+const app = express();
+const indexRouter = require('./routers/indexRouter');
+const newRouter = require('./routers/newRouter');
+const port = process.env.PORT;
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use('/', indexRouter);
+app.use('/new', newRouter);
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
